6b253e17907244d85c6b8e2abfba1756baed4aad
jiaqi.liu <2584316041@qq.com>
2018-12-17 23:01:05 +0800

    django分页

---
+++ myblog/views.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, InvalidPage
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def paginator_view(request):
+    book_list = []
+    '''
+    数据通常是从 models 中获取。这里为了方便，直接使用生成器来获取数据。
+    '''
+    for x in range(1, 260):  # 一共 25 本书
+        book_list.append('Book ' + str(x))
+
+    # 将数据按照规定每页显示 10 条, 进行分割
+    paginator = Paginator(book_list, 10)
+
+    if request.method == "GET":
+        # 获取 url 后面的 page 参数的值, 首页不显示 page 参数, 默认值是 1
+        page = request.GET.get('page')
+        try:
+            books = paginator.page(page)
+        # todo: 注意捕获异常
+        except PageNotAnInteger:
+            # 如果请求的页数不是整数, 返回第一页。
+            books = paginator.page(1)
+        except InvalidPage:
+            # 如果请求的页数不存在, 重定向页面
+            return HttpResponse('找不到页面的内容')
+        except EmptyPage:
+            # 如果请求的页数不在合法的页数范围内，返回结果的最后一页。
+            books = paginator.page(paginator.num_pages)
+
+    template_view = 'page.html'
+    return render(request, template_view, {'books': books})
+
